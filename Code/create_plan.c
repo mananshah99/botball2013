0a8a5af25375f0b62abc2394938c7eebfcc19972
Manan Shah <manan.shah.777@gmail.com>
2013-04-04 12:11:22 -0700

    Update create_plan.c

--- Code/create_plan.c
+++ Code/create_plan.c
@@ -2,18 +2,25 @@
 #define aligndist 150
 #include "createDrive.c"
 
+void drive_out()
+{
+	create_drive_direct_dist(350,350,90); // drive out of the starting position
+	create_drive_direct_right(300,500,20); // first turn right
+	create_drive_direct_right(100,500,80); // complete the turn right and end up near the first tribble box that matters	
+}
+
 int main()
 {
 	create_special_connect(); // because create_connect didn't want to work
-	create_drive_direct_dist(350,350,90); // drive out of the starting position
-	create_drive_direct_right(300,500,20); // first turn right
-	create_drive_direct_right(100,500,80); // complete the turn right and end up near the first tribble box that matters
+	drive_out();
 	create_block();
 	msleep(2000); // simulates the create picking up the tribbles
 	create_drive_direct_left(100,2,55); // move to the next pile
 	create_block();
-	create_backward(20,200); // goes to the next pile of tribbles
+	create_backward(20,200); // backs up so the claw can fit properly
 	msleep(2000); //waits, simulating the create picking up tribbles
+	
+	
 	
 	/*
 	create_drive_direct(500,500); // for right now, this is the create driving on the main road
